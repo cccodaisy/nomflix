43e49fd048182255a5668ba4c875c46b29fab5e2
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2019-12-18 21:32:55 +0900

    #1.4 Location Aware Header

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,14 +8,14 @@ import Header from "Components/Header";
 export default () => (
     <Router>
         <>
-        <Header/>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" component={TV} />
-            {/* <Route path="/tv/popular" render={() => <h1>Popular</h1>} /> */}
-            <Route path="/search" component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
+            <Header/>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" component={TV} />
+                {/* <Route path="/tv/popular" render={() => <h1>Popular</h1>} /> */}
+                <Route path="/search" component={Search} />
+                <Redirect from="*" to="/" />
+            </Switch>
         </>
     </Router>
 )

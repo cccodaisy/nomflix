55fe786d98e6afc0041d5a3c4a135f30eeca72e6
cccodaisy <58411126+cccodaisy@users.noreply.github.com>
2019-12-29 21:41:10 +0900

    #2.4 Presenter of All Routes

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -3,12 +3,12 @@ import DetailPresenter from "./DetailPresenter";
 import { moviesApi, tvApi } from "../../api";
 
 export default class extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     const {
-      location: {pathname}
+      location: { pathname }
     } = props;
-    this.statem= {
+    this.state = {
       result: null,
       error: null,
       loading: true,
@@ -19,27 +19,26 @@ export default class extends React.Component {
   async componentDidMount() {
     const {
       match: {
-        params: {id}
+        params: { id }
       },
-      history: {push}
+      history: { push }
     } = this.props;
     const { isMovie } = this.state;
-    //this.isMovie = pathname.includes("/movie/");
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
       return push("/");
     }
     let result = null;
     try {
-      if(isMovie){
-        ({data: result}= await moviesApi.movieDetail(parsedId))
+      if (isMovie) {
+        ({ data: result } = await moviesApi.movieDetail(parsedId));
       } else {
-        ({data: result} = await tvApi.showDetail(parsedId))
+        ({ data: result } = await tvApi.showDetail(parsedId));
       }
-    }catch{
-      this.setState({error: "Can't find anything."});
-    }finally{
-      this.setState({loading: false, result});
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
     }
   }
 
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,89 @@
-export default () => "Search";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Section from "Components/Section"; 
+import Loader from "Components/Loader";
+import Message from "Components/Message";
+import Poster from "Components/Poster";
+
+const Container = styled.div`
+    padding: 0 20px;
+`;
+
+const Form = styled.form`
+    maring-bottom: 50px;
+    width: 100%;
+`;
+
+const Input = styled.input`
+    all: unset;
+    font-size: 28px;
+    width: 100%;
+`;
+
+const SearchPresenter = ({
+    movieResults, tvResults, searchTerm, updateTerm, handleSubmit, loading, error
+}) => <Container>
+    <Form onSubmit={handleSubmit}>
+        <Input 
+            placeholder="Search Movies or TV shows..." 
+            value={searchTerm}
+            onChange={updateTerm}
+        />
+    </Form>
+    {loading ? (
+        <Loader />
+    ) : (
+        <>
+            {movieResults && movieResults.length > 0 && (
+                <Section title="Movie Results">
+                    {movieResults.map(movie => (
+                        <Poster 
+                        key={movie.id}
+                        id={movie.id}
+                        imageUrl={movie.poster_path}
+                        title={movie.original_title} 
+                        rating={movie.vote_average}
+                        year={movie.release_date && movie.release_date.substring(0,4)}
+                        isMovie={true}
+                        />
+                    ))}
+                </Section>
+            )}
+            {tvResults && tvResults.length > 0 && (
+                <Section title="TV Show Results">
+                    {tvResults.map(show => (
+                        <Poster 
+                        key={show.id}
+                        id={show.id}
+                        imageUrl={show.poster_path}
+                        title={show.original_name} 
+                        rating={show.vote_average}
+                        year={show.first_air_date.substring(0,4)}
+                        />
+                    ))}
+                </Section>
+            )}
+            {error && <Message color="#e74c3c" text={error} />}
+            {tvResults && 
+            movieResults && 
+            tvResults.length === 0 && 
+            movieResults.length === 0 && (
+                <Message text="Nothing found" color="#95a5a6" />
+                // <Message text={`Nothing found for: ${searchTerm}`} color="#95a5a6" />
+            )}
+        </>
+    )}
+</Container>;
+
+SearchPresenter.propTypes = {
+    movieResults: PropTypes.array,
+    tvResults: PropTypes.array,
+    searchTerm: PropTypes.string,
+    updateTerm: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.string
+};
+
+export default SearchPresenter;
